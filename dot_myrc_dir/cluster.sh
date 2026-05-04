9413eab9512ca9634d974c627a77ff79d5f9543a
Nicolo Massari <nicolo.massari@protonmail.com>
2026-05-04 20:19:07 +0200

    feat: join existing jupyterhub

--- dot_myrc_dir/cluster.sh
+++ dot_myrc_dir/cluster.sh
@@ -8,3 +8,32 @@ fi
 
 # connect runs locally (no srun needed)
 remotedev() { _cluster connect "$@"; }
+
+# easy cli
+srun_lam() {
+  local cmd="bash"
+  local cpus=16 gpus=0 mem="32G" time="02:00:00"
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -c|--cpus)  cpus=$2;  shift 2 ;;
+      -g|--gpus)  gpus=$2;  shift 2 ;;
+      -m|--mem)   mem=$2;   shift 2 ;;
+      -t|--time)  time=$2;  shift 2 ;;
+      *)          cmd="$*"; break   ;;
+    esac
+  done
+
+  local gpu_flag=""
+  [[ $gpus -gt 0 ]] && gpu_flag="--gres=gpu:$gpus"
+
+  ssh -t lam "
+    node=\$(squeue --me --noheader --format='%N' --states=RUNNING | head -1)
+    if [[ -z \"\$node\" ]]; then
+      echo 'No running allocation, requesting one...'
+      node=\$(salloc --no-shell --cpus-per-task=$cpus $gpu_flag --mem=$mem --time=$time 2>&1 | grep -oP 'node\K\S+' | head -1)
+    fi
+    echo \"Jumping to \$node\"
+    ssh -t \"\$node\" 'cd $(pwd) && $cmd'
+  "
+}
